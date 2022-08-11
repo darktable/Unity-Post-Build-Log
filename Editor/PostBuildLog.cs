ff9d6c11e2375d90f96761120ba1023cefbb83e9
calvin rien <calvin@darktable.com>
2022-08-11 09:52:00 -0700

    Apply Unity coding conventions.

--- Editor/PostBuildLog.cs
+++ Editor/PostBuildLog.cs
@@ -1,307 +1,347 @@
 #define GIT_CHECK
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Text.RegularExpressions;
 using Debug = UnityEngine.Debug;
 
-public class PostBuildLog : ScriptableObject {
-    private static readonly Regex ASSET_ENTRY = new Regex(@"^.*% (Assets/.*)$", RegexOptions.IgnoreCase);
-    private static readonly Regex GIT_ERROR = new Regex("^error:.*'(.*)'.*$", RegexOptions.IgnoreCase);
+public class PostBuildLog : ScriptableObject
+{
+    static readonly Regex k_AssetEntry = new Regex(@"^.*% (Assets/.*)$", RegexOptions.IgnoreCase);
+    static readonly Regex k_GitError = new Regex("^error:.*'(.*)'.*$", RegexOptions.IgnoreCase);
 
-    private const string GIT_LS = "ls-files --error-unmatch{0}";
+    const string k_GitLs = "ls-files --error-unmatch{0}";
 
-	private enum STATUS
-	{
-		FOUND,
-		END
-	}
+    enum Status
+    {
+        Found,
+        End
+    }
 
-	private static readonly Regex BUILD_REPORT = new Regex(@"^Build Report$", RegexOptions.IgnoreCase);
-	private static readonly Regex DASH_LINE = new Regex(@"^-+$");
-	private static readonly Regex DEPENDENCIES_LIST = new Regex(@"^Mono dependencies included in the build$", RegexOptions.IgnoreCase);
+    static readonly Regex k_BuildReport = new Regex(@"^Build Report$", RegexOptions.IgnoreCase);
+    static readonly Regex k_DashLine = new Regex(@"^-+$");
+    static readonly Regex k_DependenciesList = new Regex(@"^Mono dependencies included in the build$", RegexOptions.IgnoreCase);
 
-	[MenuItem("Tools/Post Build Log/Test Build Report")]
-	private static void TestBuildReport()
-	{
-		WriteBuildLog(Application.dataPath, "android");
-	}
+    [MenuItem("Tools/Post Build Log/Test Build Report")]
+    static void TestBuildReport()
+    {
+        WriteBuildLog(Application.dataPath, "android");
+    }
 
-	[PostProcessBuild] // Requires Unity 3.5+
-    private static void OnPostProcessBuildPlayer(BuildTarget target, string buildPath) {
-		// This is a hack, but on windows you have to wait
-		// 1 frame after a build finishes for the
-		// log file to get written out. (and delayCall doesn't work).
-		EditorApplication.update = CallbackFunc;
+    [PostProcessBuild] // Requires Unity 3.5+
+    static void OnPostProcessBuildPlayer(BuildTarget target, string buildPath)
+    {
+        // This is a hack, but on windows you have to wait
+        // 1 frame after a build finishes for the
+        // log file to get written out. (and delayCall doesn't work).
+        EditorApplication.update = CallbackFunc;
 
-		void CallbackFunc()
-		{
-			WriteBuildLog(buildPath, target.ToString());
-			EditorApplication.update -= CallbackFunc;
-		}
-	}
+        void CallbackFunc()
+        {
+            WriteBuildLog(buildPath, target.ToString());
+            EditorApplication.update -= CallbackFunc;
+        }
+    }
 
-	private static string ScenesInBuild()
-	{
-		StringBuilder scenesList = new StringBuilder();
+    static string ScenesInBuild()
+    {
+        StringBuilder scenesList = new StringBuilder();
 
-		scenesList.AppendLine("Scenes included in the build");
+        scenesList.AppendLine("Scenes included in the build");
 
-		int sceneCount = SceneManager.sceneCountInBuildSettings;
-		for (int i = 0; i < sceneCount; i++)
-		{
-			var sceneName = SceneUtility.GetScenePathByBuildIndex(i);
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneCount; i++)
+        {
+            var sceneName = SceneUtility.GetScenePathByBuildIndex(i);
 
-			scenesList.AppendLine(sceneName);
-		}
+            scenesList.AppendLine(sceneName);
+        }
 
-		return scenesList.ToString();
-	}
+        return scenesList.ToString();
+    }
 
-    private static void WriteBuildLog(string buildPath, string target="") {
+    static void WriteBuildLog(string buildPath, string target = "")
+    {
         //string editorLogFilePath = null;
         string[] pieces;
 
         bool winEditor = Application.platform == RuntimePlatform.WindowsEditor;
-		STATUS buildMarker = STATUS.END;
-		STATUS dependencyMarker = STATUS.END;
+        Status buildMarker = Status.End;
+        Status dependencyMarker = Status.End;
 
-        if (winEditor) {
-			pieces = new string[] { Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-				"Unity", "Editor", "Editor.log"};
-        } else {
-            pieces = new string[] { Environment.GetFolderPath(Environment.SpecialFolder.Personal),
-				"Library","Logs","Unity","Editor.log"};
+        if (winEditor)
+        {
+            pieces = new string[]
+            {
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "Unity", "Editor", "Editor.log"
+            };
+        }
+        else
+        {
+            pieces = new string[]
+            {
+                Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                "Library", "Logs", "Unity", "Editor.log"
+            };
         }
 
-		string editorLogFilePath = Path.Combine(pieces);
+        string editorLogFilePath = Path.Combine(pieces);
 
-        if (!File.Exists(editorLogFilePath)) {
+        if (!File.Exists(editorLogFilePath))
+        {
             Debug.LogWarning("Editor log file could not be found at: " + editorLogFilePath);
             return;
         }
 
         StringBuilder report = new StringBuilder();
-		StringBuilder assemblies = new StringBuilder();
+        StringBuilder assemblies = new StringBuilder();
 
-        using (StreamReader reader = new StreamReader(File.Open(editorLogFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))) {
-			string prevLine = null;
-			string currentLine = null;
-			int lineNum = 0;
-            while (true) {
-				lineNum++;
-				prevLine = currentLine;
+        using (StreamReader reader = new StreamReader(File.Open(editorLogFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+        {
+            string currentLine = null;
+            int lineNum = 0;
+            while (true)
+            {
+                lineNum++;
+                var prevLine = currentLine;
                 currentLine = reader.ReadLine();
 
-                if (currentLine == null) {
+                if (currentLine == null)
+                {
                     break;
                 }
 
-				if (BUILD_REPORT.IsMatch(currentLine)) {
-					Debug.LogFormat("found a build report at line: {0}", lineNum);
+                if (k_BuildReport.IsMatch(currentLine))
+                {
+                    Debug.LogFormat("found a build report at line: {0}", lineNum);
 
-					buildMarker = STATUS.FOUND;
+                    buildMarker = Status.Found;
                     report.Length = 0;
-					report.AppendLine(prevLine);
+                    report.AppendLine(prevLine);
                 }
-				else if (DEPENDENCIES_LIST.IsMatch(currentLine))
-				{
-					dependencyMarker = STATUS.FOUND;
-					assemblies.Length = 0;
-					assemblies.AppendLine(prevLine);
-				}
+                else if (k_DependenciesList.IsMatch(currentLine))
+                {
+                    dependencyMarker = Status.Found;
+                    assemblies.Length = 0;
+                    assemblies.AppendLine(prevLine);
+                }
 
-				if (buildMarker == STATUS.FOUND)
-				{
-					report.AppendLine(currentLine);
-				}
-				else if (dependencyMarker == STATUS.FOUND)
-				{
-					assemblies.AppendLine(currentLine);
-				}
+                if (buildMarker == Status.Found)
+                {
+                    report.AppendLine(currentLine);
+                }
+                else if (dependencyMarker == Status.Found)
+                {
+                    assemblies.AppendLine(currentLine);
+                }
 
-				// end of report.
-				if (buildMarker == STATUS.FOUND && DASH_LINE.IsMatch(currentLine))
-				{
-					buildMarker = STATUS.END;
-				}
-				else if (dependencyMarker == STATUS.FOUND && string.IsNullOrEmpty(currentLine))
-				{
-					dependencyMarker = STATUS.END;
-				}
+                // end of report.
+                if (buildMarker == Status.Found && k_DashLine.IsMatch(currentLine))
+                {
+                    buildMarker = Status.End;
+                }
+                else if (dependencyMarker == Status.Found && string.IsNullOrEmpty(currentLine))
+                {
+                    dependencyMarker = Status.End;
+                }
             }
         }
 
-        if (report.Length == 0) {
-			Debug.Log("no build report found in log.");
-			return; // No builds have been run.
+        if (report.Length == 0)
+        {
+            Debug.Log("no build report found in log.");
+            return; // No builds have been run.
         }
 
         string outputPath;
 
-		var filename = string.Format("build {0}.log", System.DateTime.UtcNow.ToString("s").Replace(':', '-'));
+        var filename = $"build {DateTime.UtcNow.ToString("s").Replace(':', '-')}.log";
 
-		if (target.StartsWith("standalone", StringComparison.InvariantCultureIgnoreCase) || target.StartsWith("android", StringComparison.InvariantCultureIgnoreCase)) {
-			outputPath = Path.Combine(Path.GetDirectoryName(buildPath), filename);
-        } else {
-			outputPath = Path.Combine(buildPath, filename);
+        if (target.StartsWith("standalone", StringComparison.InvariantCultureIgnoreCase) || target.StartsWith("android", StringComparison.InvariantCultureIgnoreCase))
+        {
+            outputPath = Path.Combine(Path.GetDirectoryName(buildPath), filename);
+        }
+        else
+        {
+            outputPath = Path.Combine(buildPath, filename);
         }
 
-        try {
-			var output = new StringBuilder();
-			output.AppendFormat("Build Report @ {0}\n\n", System.DateTime.Now.ToString("u"));
-			output.Append(ScenesInBuild());
-			output.Append(assemblies.ToString());
-			output.Append(report.ToString());
+        try
+        {
+            var output = new StringBuilder();
+            output.AppendFormat("Build Report @ {0:u}\n\n", DateTime.Now);
+            output.Append(ScenesInBuild());
+            output.Append(assemblies.ToString());
+            output.Append(report);
 
-			File.WriteAllText(outputPath, output.ToString());
-        } catch (Exception e) {
-			Debug.LogException(e);
-			Debug.LogErrorFormat("Build log file could not be created for writing at: {0} for target {1}", outputPath, target);
+            File.WriteAllText(outputPath, output.ToString());
+
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+            Debug.LogErrorFormat("Build log file could not be created for writing at: {0} for target {1}", outputPath, target);
         }
 
 #if GIT_CHECK
-		CheckGit(report.ToString());
+        CheckGit(new StringReader(report.ToString()));
 #endif
-	}
+    }
 
-    private static void CheckGit(string buildReport)
-	{
-		var buildAssets = new List<string>();
+    static void CheckGit(StringReader reader)
+    {
+        // TODO: check submodules
+        // TODO: check Packages
 
-		var unversioned = new List<string>();
+        var buildAssets = new List<string>();
 
-		var lines = buildReport.Split(new[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        var unversioned = new List<string>();
 
-		var directoryHashset = new HashSet<string>();
+        var directoryHashset = new HashSet<string>();
 
-		// for each line in buildreport
-		// see if it matches regex
-		// if so add the matched substring to assets
-		foreach (var line in lines)
-		{
-			var match = ASSET_ENTRY.Match(line);
+        while (reader.Peek() != -1)
+        {
+            var line = reader.ReadLine();
 
-			if (match.Groups.Count == 2)
-			{
-				var asset = match.Groups[1].Value;
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
 
-				var path = Path.GetDirectoryName(asset).Replace('\\', '/');
+            // var lines = buildReport.Split(new[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
-				if (!path.Equals("Assets", StringComparison.CurrentCultureIgnoreCase))
-				{
-					directoryHashset.Add(string.Format("{0}.meta", path));
-				}
+            // for each line in buildreport
+            // see if it matches regex
+            // if so add the matched substring to assets
+            var match = k_AssetEntry.Match(line);
 
-				buildAssets.Add(asset);
-			}
-		}
+            if (match.Groups.Count == 2)
+            {
+                var asset = match.Groups[1].Value;
 
-		var arguments = new List<string>();
+                if (string.IsNullOrEmpty(asset))
+                {
+                    continue;
+                }
 
-		var stringBuilder = new StringBuilder();
+                var path = Path.GetDirectoryName(asset).Replace('\\', '/');
 
-		foreach (var asset in buildAssets)
-		{
-			// Verify the file actually exists
-			if (!File.Exists(asset))
-			{
-				// Unity generates some assets that are added to the build.
-				// (Unity does this with movies)
+                if (!path.Equals("Assets", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    directoryHashset.Add(path);
+                }
 
-				//Debug.LogWarningFormat("doesn't exist: {0}", asset);
-				continue;
-			}
+                buildAssets.Add(asset);
+            }
+        }
 
-			// also check for .meta files
-			var line = string.Format(" \"{0}\" \"{0}.meta\"", asset);
+        var arguments = new List<string>();
 
-			if (line.Length + stringBuilder.Length > 2000)
-			{
-				arguments.Add(stringBuilder.ToString());
-				stringBuilder.Clear();
-			}
+        var stringBuilder = new StringBuilder();
 
-			stringBuilder.Append(line);
-		}
+        foreach (var asset in buildAssets)
+        {
+            // Verify the file actually exists
+            if (!File.Exists(asset))
+            {
+                // Unity generates some assets that are added to the build.
+                // (Unity does this with movies)
 
-		arguments.Add(stringBuilder.ToString());
-		stringBuilder.Clear();
+                //Debug.LogWarningFormat("doesn't exist: {0}", asset);
+                continue;
+            }
 
-		foreach (var dir in directoryHashset)
-		{
-			var line = string.Format(" \"{0}\"", dir);
+            // also check for .meta files
+            var line = string.Format(" \"{0}\" \"{0}.meta\"", asset);
 
-			if (line.Length + stringBuilder.Length > 2000)
-			{
-				arguments.Add(stringBuilder.ToString());
-				stringBuilder.Clear();
-			}
+            if (line.Length + stringBuilder.Length > 2000)
+            {
+                arguments.Add(stringBuilder.ToString());
+                stringBuilder.Clear();
+            }
 
-			stringBuilder.Append(line);
-		}
+            stringBuilder.Append(line);
+        }
 
-		arguments.Add(stringBuilder.ToString());
-		stringBuilder.Clear();
+        arguments.Add(stringBuilder.ToString());
+        stringBuilder.Clear();
 
-		foreach (var line in arguments)
-		{
-			using (var process = new System.Diagnostics.Process())
-			{
-				process.StartInfo.FileName = "git";
-				process.StartInfo.CreateNoWindow = true;
-				process.StartInfo.UseShellExecute = false;
-				//process.StartInfo.RedirectStandardOutput = true;
-				process.StartInfo.RedirectStandardError = true;
-				process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-				process.StartInfo.Arguments = string.Format(GIT_LS, line);
-				process.Start();
+        foreach (var dir in directoryHashset)
+        {
+            var line = $" \"{dir}.meta\"";
 
-				process.WaitForExit();
+            if (line.Length + stringBuilder.Length > 2000)
+            {
+                arguments.Add(stringBuilder.ToString());
+                stringBuilder.Clear();
+            }
 
-				if (process.ExitCode != 0)
-				{
-					var err = process.StandardError.ReadToEnd();
+            stringBuilder.Append(line);
+        }
 
-					var results = err.Split(new[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        arguments.Add(stringBuilder.ToString());
+        stringBuilder.Clear();
 
-					foreach (var result in results)
-					{
-						var match = GIT_ERROR.Match(result);
+        foreach (var line in arguments)
+        {
+            using var process = new Process();
+            process.StartInfo.FileName = "git";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
 
-						if (match.Groups.Count == 2)
-						{
-							var unver = match.Groups[1].Value;
+            //process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.Arguments = string.Format(k_GitLs, line);
+            process.Start();
 
-							//Debug.LogWarningFormat("unversioned asset in build: {0}", unver);
+            process.WaitForExit();
 
-							unversioned.Add(unver);
-						}
-					}
-				}
-			}
-		}
+            if (process.ExitCode != 0)
+            {
+                var err = process.StandardError.ReadToEnd();
 
-		if (unversioned.Count == 0)
-		{
-			Debug.Log("No unversioned assets in build!");
-		}
-		else
-		{
-			unversioned.Sort();
+                var results = err.Split(new[] { Environment.NewLine, "\n" }, StringSplitOptions.RemoveEmptyEntries);
 
-			foreach (var asset in unversioned)
-			{
-				Debug.LogWarningFormat("unversioned asset in build: {0}", asset);
-			}
-		}
+                foreach (var result in results)
+                {
+                    var match = k_GitError.Match(result);
 
-		// TODO: Show a dialog box and add the assets to git?
-	}
+                    if (match.Groups.Count == 2)
+                    {
+                        var unver = match.Groups[1].Value;
+
+                        //Debug.LogWarningFormat("unversioned asset in build: {0}", unver);
+
+                        unversioned.Add(unver);
+                    }
+                }
+            }
+        }
+
+        if (unversioned.Count == 0)
+        {
+            Debug.Log("No unversioned assets in build!");
+        }
+        else
+        {
+            unversioned.Sort();
+
+            foreach (var asset in unversioned)
+            {
+                Debug.LogWarningFormat("unversioned asset in build: {0}", asset);
+            }
+        }
+
+        // TODO: Show a dialog box and add the assets to git?
+    }
 }
